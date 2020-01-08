274ebe2e0126dc9a2f2422015f7d0742202c18d6
Artem Dunaev <dunaev.aa@yandex.ru>
2020-01-08 22:39:17 +0500

    make generateProgressionArray pure function

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,25 +2,22 @@ import generateRandomInteger from '../utils';
 
 const MIN_DELTA_INTEGER = -10;
 const MAX_DELTA_INTEGER = 10;
-
 const PROGRESSION_SIZE = 10;
 
 const description = 'What number is missing in the progression?\n';
 
-const generatePregressionArray = () => {
-  const delta = generateRandomInteger(MIN_DELTA_INTEGER, MAX_DELTA_INTEGER);
-  const startNumber = generateRandomInteger();
-
+const generateProgressionArray = (startNumber, delta, size) => {
   const result = [];
-  for (let i = 0; i < PROGRESSION_SIZE; i += 1) {
+  for (let i = 0; i < size; i += 1) {
     result.push(startNumber + i * delta);
   }
-
   return result;
 };
 
 const makeGame = () => {
-  const progression = generatePregressionArray();
+  const startNumber = generateRandomInteger();
+  const delta = generateRandomInteger(MIN_DELTA_INTEGER, MAX_DELTA_INTEGER);
+  const progression = generateProgressionArray(startNumber, delta, PROGRESSION_SIZE);
   const randomIndex = generateRandomInteger(0, progression.length);
   const progressionWithoutNumber = progression
     .map((number, index) => (index === randomIndex ? '..' : number));
